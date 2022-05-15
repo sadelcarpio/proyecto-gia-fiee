f5b26ec8cacf5e74e6d1cd641c893bca8935b32e
sadelcarpio <sadelcarpioa@gmail.com>
2022-05-15 17:31:37 -0500

    Se implemento el TabBarView. Revisar cada uno sus archivos

---
+++ lib/screens/objects/objects.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+// Jean
+class ObjectsRecognitionPage extends StatelessWidget {
+  const ObjectsRecognitionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/persons/persons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+// David
+class PersonRecognitionPage extends StatelessWidget {
+  const PersonRecognitionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
